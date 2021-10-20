895e237aac1a7aa0f7a367998eb08ff8b0557186
ziwei <iamzhengziwei@gmail.com>
2021-10-20 11:31:50 +0800

    修改公式js

--- public/js/katex.js
+++ public/js/katex.js
@@ -24,9 +24,7 @@
   if (config.delimiters === "true") {
     document.addEventListener("DOMContentLoaded", function () {
       renderMathInElement(document.body, {
-        delimiters: [
-          { left: "$$", right: "$$", display: true },
-          { left: "$", right: "$", display: false }
+        delimiters: [{left: "$$", right: "$$", display: true}, { left: "$", right: "$", display: false }, {left: "\\(", right: "\\)", display: false}, {left: "\\[", right: "\\]", display: true}
         ]
       });
     });
